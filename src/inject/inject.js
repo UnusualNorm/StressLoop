79816d7c80d4e8d8d740636d191a39d69ff382ca
UnusualN0rm <40722525+UnusualN0rm@users.noreply.github.com>
2020-09-01 16:31:34 -0400

    0ms

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -14,7 +14,7 @@ chrome.extension.sendMessage({}, function(response) {
 					} else {
 						$("[name|=\"host\"]").typetype(urlParams.get("ip"), {
 							e: 0,
-							t: 1
+							t:0
 						  });
 						if (urlParams.has("method")) {
 							$('select>option:eq(' + urlParams.get("method") + ')').prop('selected', true);
@@ -24,23 +24,23 @@ chrome.extension.sendMessage({}, function(response) {
 						if (urlParams.has("port")) {
 							$("[name|=\"port\"]").typetype(urlParams.get("port"), {
 								e: 0,
-								t: 1
+								t: 0
 							  });
 						} else {
 							$("[name|=\"port\"]").typetype("80", {
 								e: 0,
-								t: 1
+								t: 0
 							  });
 						}
 						if (urlParams.has("time")) {
 							$("[name|=\"time\"]").typetype(urlParams.get("ip"), {
 								e: 0,
-								t: 1
+								t: 0
 							  });
 						} else {
 							$("[name|=\"time\"]").typetype("240", {
 								e: 0,
-								t: 1
+								t: 0
 							  });
 						}
 						setTimeout(() => {
