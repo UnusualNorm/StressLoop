cf7fe0f77f9ac6de22c14992fe1cf318de4d993e
UnusualN0rm <40722525+UnusualN0rm@users.noreply.github.com>
2020-09-01 13:24:23 -0400

    Initial commit

---
+++ src/inject/inject.js
@@ -0,0 +1,61 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+		if (document.readyState === "complete") {
+			clearInterval(readyStateCheckInterval);
+			
+			const urlParams = new URLSearchParams(window.location.search);
+			
+			if (urlParams.has("autoboot") == true) {
+				if (urlParams.has("ip")) {
+					if ($("td.green").length) {
+						setTimeout(() => {
+							location.reload();
+						}, 5000);
+					} else {
+						$("[name|=\"host\"]").typetype(urlParams.get("ip"), {
+							e: 0,
+							t: 1
+						  });
+						if (urlParams.has("method")) {
+							$('select>option:eq(' + urlParams.get("method") + ')').prop('selected', true);
+						} else {
+							$('select>option:eq(1)').prop('selected', true);
+						}
+						if (urlParams.has("port")) {
+							$("[name|=\"port\"]").typetype(urlParams.get("port"), {
+								e: 0,
+								t: 1
+							  });
+						} else {
+							$("[name|=\"port\"]").typetype("80", {
+								e: 0,
+								t: 1
+							  });
+						}
+						if (urlParams.has("time")) {
+							$("[name|=\"time\"]").typetype(urlParams.get("ip"), {
+								e: 0,
+								t: 1
+							  });
+						} else {
+							$("[name|=\"time\"]").typetype("240", {
+								e: 0,
+								t: 1
+							  });
+						}
+						setTimeout(() => {
+							$(".jssubmit").click();	
+							setTimeout(() => {
+								location.reload();
+							}, 1000);
+						}, 1000);
+					}
+					
+				} else {
+					alert("REQUIRES IP!");
+				}
+				
+			}
+		}
+	}, 10);
+});
