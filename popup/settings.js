966851556aee445d09313e1fdf5179d5cdd22cee
UnusualN0rm <40722525+UnusualN0rm@users.noreply.github.com>
2020-09-02 21:03:01 -0400

    FIXED IT!!!

--- popup/settings.js
+++ popup/settings.js
@@ -1,26 +1,20 @@
 chrome.runtime.sendMessage({subject: "settings"}, function(response) {
     refSecondDelay = response.refDelay / 1000;
     document.getElementById("refDelay").placeholder = refSecondDelay + "s";
-    refMiliDelay = document.getElementById("clickDelay").value * 1000;
-
 
     startSecondDelay = response.startDelay / 1000;
     document.getElementById("startDelay").placeholder = startSecondDelay + "s";
-    startMiliDelay = document.getElementById("clickDelay").value * 1000;
-
 
     clickSecondDelay = response.clickDelay / 1000;
     document.getElementById("clickDelay").placeholder = clickSecondDelay + "s";
-    clickMiliDelay = document.getElementById("clickDelay").value * 1000;
-
 
 
     document.getElementById("save").onclick = function(){
         chrome.runtime.sendMessage({
             subject: "updateSettings",
-            refDelay: refMiliDelay,
-            startDelay: startMiliDelay,
-            clickDelay: clickMiliDelay
+            refDelay: document.getElementById("refDelay").value * 1000,
+            startDelay: document.getElementById("startDelay").value * 1000,
+            clickDelay: document.getElementById("clickDelay").value * 1000
         });
     };
 });
